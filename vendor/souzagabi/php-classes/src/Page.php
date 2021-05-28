98b03fa49e7513e5f767e559b47cc5d9c7d5eaed
souzagabi <souza_gabi@yahoo.com.br>
2021-05-28 17:36:27 -0300

    mudaças na view

--- vendor/souzagabi/php-classes/src/Page.php
+++ vendor/souzagabi/php-classes/src/Page.php
@@ -1,5 +1,5 @@
 <?php
-    namespace Acao;
+    namespace PRJM010;
     use Rain\Tpl;
 
     class Page{
@@ -14,29 +14,22 @@
         {
             $this->options = array_merge($this->defaults, $opts);
 
-            $config = array(
-               "tpl_dir"    => $_SERVER['DOCUMENT_ROOT'].$tpl_dir, 
-               "cache_dir"  => $_SERVER['DOCUMENT_ROOT']."/views-cache/", 
-               "debug"      => false           
-            );
-            Tpl::configure( $config );
-
-            $this->tpl = new Tpl;
-
-            $this->setData($this->options["data"]);
+            $this->setConfig("/views/");
 
             if($this->options["header"] === true){ 
-                $this->tpl->draw("header");
+                $this->setTpl("header", $this->options["data"]);
             }
+            
+            $this->setConfig($tpl_dir);
         }
-
+        
         private function setData($data = array())
         {
             foreach ($data as $key => $value) {
                 $this->tpl->assign($key, $value);
-             }
+            }
         }
-
+        
         public function setTpl($name, $data = array(), $returnHTML = false)
         {
             $this->setData($data);
@@ -45,10 +38,25 @@
         
         public function __destruct()
         {
+            $this->setConfig("/views/");
+             
             if($this->options["footer"] === true) 
             {
                 $this->tpl->draw("footer");
             }
+        }
+        private function setConfig($tpl_dir)
+        {
+            //Configura o diretório raiz para carregar header,o footer e os contents
+            $config = array(
+                "tpl_dir"    => $_SERVER['DOCUMENT_ROOT'].$tpl_dir, 
+                "cache_dir"  => $_SERVER['DOCUMENT_ROOT']."/views-cache/", 
+                "debug"      => false           
+             );
+             
+             Tpl::configure( $config );
+ 
+             $this->tpl = new Tpl;
         }
     }
 ?>
